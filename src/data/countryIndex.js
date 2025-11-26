dabc18ca1ee611084db01f19f4c13148128fcd1a
inaeLee1229 <iinae363@gmail.com>
2025-11-26 17:46:42 +0900

    countrysearch

--- src/data/countryIndex.js
+++ src/data/countryIndex.js
@@ -1,15 +1,17 @@
 // src/data/countryIndex.js
-const countryIndex = [
-  { code: "KR", keywords: ["한국", "대한민국", "South Korea", "Korea", "KR"] },
-  { code: "JP", keywords: ["일본", "Japan", "JP", "JP1", "JP2"] },
-  { code: "CN", keywords: ["중국", "China", "CN"] },
-  { code: "US", keywords: ["미국", "United States", "USA", "US", "United States of America"] },
-  { code: "GB", keywords: ["영국", "United Kingdom", "UK", "Britain", "GB"] },
-  { code: "FR", keywords: ["프랑스", "France", "FR"] },
-  { code: "DE", keywords: ["독일", "Germany", "DE"] },
-  { code: "ES", keywords: ["스페인", "Spain", "ES"] },
-  { code: "IT", keywords: ["이탈리아", "Italy", "IT", "IT1", "IT2"] },
-  { code: "KZ", keywords: ["카자흐스탄", "Kazakhstan", "KZ"] },
-];
+
+import countryData from "./countryData";
+
+const countryIndex = Object.entries(countryData).map(([code, info]) => {
+  const keywords = [
+    info.name, // 한국어 이름 
+    info.englishName, // 영문 이름 
+    code, // ISO 코드 
+    ...(info.aliases || []) // 별칭 있으면 추가
+  ].filter(Boolean);
+
+  return { code, keywords };
+});
 
 export default countryIndex;
+
